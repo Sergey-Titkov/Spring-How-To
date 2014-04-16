d6c9a0ce36a8e9a5cdf6bd93935dba1ddf95101d
Sergey.Titkov <Sergey.Titkov@billing.ru>
2014-04-16 12:39:21 +0400

    Пример готов

--- Undestand value/src/main/java/com/springapp/mvc/HelloController.java
+++ Undestand value/src/main/java/com/springapp/mvc/HelloController.java
@@ -1,5 +1,6 @@
 package com.springapp.mvc;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,9 +9,24 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/")
 public class HelloController {
+
+  // Получение значений "системных" свойств. Речь идет о томкате и свойствах в файле catalina.proprties
+  // Получаем влоб, да
+  @Value("#{systemProperties['ucs.scc.pool.url']}")
+  private String url;
+
+  // Для того что бы этот подход роботал необходимо добавить в xml файл контекста строку
+  // <context:property-placeholder /> и все, все системные параметры будут доступны.
+  // Приятный бонус! Если определен локальный файл настроек, то параметры в нем будут перекрыты
+  // системными параметрами.
+  @Value("${ucs.scc.pool.url}")
+  private String url_placeholder;
+
 	@RequestMapping(method = RequestMethod.GET)
 	public String printWelcome(ModelMap model) {
 		model.addAttribute("message", "Hello world!");
+    model.addAttribute("url", url);
+    model.addAttribute("url_placeholder", url_placeholder);
 		return "hello";
 	}
 }
--- Undestand value/src/main/webapp/WEB-INF/pages/hello.jsp
+++ Undestand value/src/main/webapp/WEB-INF/pages/hello.jsp
@@ -1,5 +1,9 @@
+<%@ page contentType="text/html; charset=UTF-8" %>
 <html>
 <body>
 	<h1>${message}</h1>
+    Напрямую ucs.scc.pool.url = ${url}<br>
+    Через Property Placeholders ucs.scc.pool.url = ${url_placeholder}<br>
+
 </body>
 </html>
